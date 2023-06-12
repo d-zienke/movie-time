b31b76c8c915f6f06e0fb159bffea55ab8d38acf
Damian Zienke <zienke.damian@gmail.com>
2023-06-12 19:06:47 +0200

    Enable changing current movie
    Clicking on a recommended movie component sets new value for `currentMovie` state

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -4,7 +4,7 @@ import Movies from "../../api/get-movies";
 import SimilarMoviesItem from "../SimilarMoviesItem/SimilarMoviesItem";
 import s from "./style.module.scss";
 
-export default function Recommendations({ movies }) {
+export default function Recommendations({ movies, changeMovie }) {
 	// limit data to 10 recommended movies
 	const data = movies.slice(0, 10);
 	// prepare array of components with recommended movies
@@ -14,6 +14,8 @@ export default function Recommendations({ movies }) {
 				key={item.id}
 				title={item.title}
 				backdrop={`${ApiConfig.images.secure_base_url}w300${item.backdrop_path}`}
+				movieObject={item}
+				handleClick={changeMovie}
 			/>
 		);
 	});
--- src/components/SimilarMoviesItem/SimilarMoviesItem.jsx
+++ src/components/SimilarMoviesItem/SimilarMoviesItem.jsx
@@ -1,10 +1,22 @@
 import s from "./style.module.scss";
 
-export default function SimilarMoviesItem({ title, backdrop }) {
+export default function SimilarMoviesItem({
+	title,
+	backdrop,
+	movieObject,
+	handleClick,
+}) {
 	return (
 		<>
 			<div className={s.similar_movie}>
-				<img src={backdrop} title={title} alt={`${title} - backdrop`} />
+				<img
+					src={backdrop}
+					title={title}
+					alt={`${title} - backdrop`}
+					onClick={() => {
+						handleClick(movieObject);
+					}}
+				/>
 			</div>
 		</>
 	);
