6ee20a256b335dee461d9ce2227d940176ffe374
Damian Zienke <zienke.damian@gmail.com>
2023-06-12 16:02:48 +0200

    Add new API call
    New function has been added to `Movies` class:
    * `fetchRecommended()` makes an API call based on the `currentMovie` to get a list of similar movies

--- src/api/get-movies.js
+++ src/api/get-movies.js
@@ -2,7 +2,8 @@
 // VITE_TMDB_ACCESS_TOKEN=your_access_token
 
 import { TEST_MODE } from "../config";
-import staticData from "./static-data.json";
+import staticDataTopRated from "./static-data_top-rated.json";
+import staticDataRecommended from "./static-data_recommended.json";
 
 const options = {
 	method: "GET",
@@ -14,11 +15,25 @@ const options = {
 
 export default class Movies {
 	static async fetchTopRated() {
-		if (TEST_MODE) return staticData.results;
+		if (TEST_MODE) return staticDataTopRated.results;
 
 		try {
 			const response = await fetch(
 				"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+				options
+			);
+			const data = await response.json();
+			return data.results;
+		} catch (error) {
+			console.log(`Error: ${error.message}`);
+		}
+	}
+	static async fetchRecommended(movieId) {
+		if (TEST_MODE) return staticDataRecommended.results;
+
+		try {
+			const response = await fetch(
+				`https://api.themoviedb.org/3/movie/${movieId}/recommendations?language=en-US&page=1`,
 				options
 			);
 			const data = await response.json();
